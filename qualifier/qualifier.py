6615e8381c705e8a8dbb67e127633b2c1252a007
YihaoGu <guyihao1997@gmail.com>
2021-07-06 18:26:34 -0400

    my solution

--- qualifier/qualifier.py
+++ qualifier/qualifier.py
@@ -9,4 +9,61 @@ def make_table(rows: List[List[Any]], labels: Optional[List[Any]] = None, center
     :param centered: If the items should be aligned to the center, else they are left aligned.
     :return: A table representing the rows passed in.
     """
-    ...
+    cntRows = len(rows)
+    cntCols = len(rows[0])
+
+    def max_width(inputs: List[List[Any]]) -> List[int]:
+        width = []
+        for i in range(len(inputs[0])):
+            columns = []
+            for j in range(len(inputs)):
+                columns.append(inputs[j][i])
+            width.append(max([len(str(ci)) for ci in columns]))
+        return width
+
+    def draw_body(inputs: List[List[Any]], width: List[int], centered: bool = False) -> str:
+        body = ''
+        for i in range(cntRows):
+            for j in range(cntCols):
+                cell = str(inputs[i][j])
+                if centered:
+                    padding = width[j] + 2 - len(cell)
+                    paddingLeft = padding // 2
+                    paddingRight = padding - paddingLeft
+                    body += '│' + ' ' * paddingLeft + cell + ' '*paddingRight
+                else:
+                    body += '│ ' + cell.ljust(width[j] + 1)
+            body += '│\n'
+        return body.rstrip()
+
+    def draw_head(width: List[int]) -> str:
+        return '┌' + '┬'.join(['─' * (wi + 2) for wi in width]) + '┐'
+
+    def draw_tail(width: List[int]) -> str: 
+        return '└' + '┴'.join(['─' * (wi + 2) for wi in width]) + '┘'
+
+    def draw_labels(labels: List[Any], width: List[int], centered: bool = False) -> str:
+        label = ''
+        for i in range(cntCols):
+            cell = str(labels[i])
+            if centered:
+                padding = width[i] + 2 -len(cell)
+                paddingLeft = padding // 2
+                paddingRight = padding - paddingLeft
+                label += '│' + ' ' * paddingLeft + cell + ' '*paddingRight
+            else:
+                label += '│ ' + cell.ljust(width[i] + 1)
+        label += '│'
+        separator = '├' + '┼'.join(['─' * (wi + 2) for wi in width]) + '┤'
+        return label + "\n" + separator
+
+    finaTable = ''
+
+    if labels:
+        width = max_width(rows + [labels])
+        finaTable = draw_head(width) + '\n' + draw_labels(labels, width, centered) + '\n' + draw_body(rows, width, centered) + '\n' + draw_tail(width)
+    else:
+        width = max_width(rows)
+        finaTable =  draw_head(width) + '\n' + draw_body(rows, width, centered) + '\n' + draw_tail(width)
+
+    return finaTable
